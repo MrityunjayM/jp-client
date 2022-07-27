53fd0f27021e11f0b1759dd4252cf3bdf4b83bc6
Mrityunjay Mishra <mrityunjaymishra@hotmail.com>
2022-07-27 20:06:49 +0530

    Changed some styling :Mrityunjay

---
+++ src/components/layout/routes.js
@@ -0,0 +1,11 @@
+export default [
+    {text:"Sign-Up", route:"/signup"},
+    {text:"LogIn", route:"/login"},
+    {text:"LogOut", route:"#logout"},
+    {text:"Add Battle", route:"/addbattle"},
+    {text:"All Battle", route:"/allbattle"},
+    {text:"Give coin to User", route:"/givecointouser"},
+    {text:"Listed Game", route:"/listedgame"},
+    {text:"Upload Image", route:"/imageuploader"},
+    {text:"Super Admin", route:"/superadmin"},
+]
